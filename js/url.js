80578a4b2c1541f3bc4ad7a722e9cc123060f2fb
youer name <you@example.com>
2018-04-13 13:45:58 +0800

    daily

--- js/url.js
+++ js/url.js
@@ -1,6 +1,6 @@
 /**
  * Created by Tina on 2018/2/7 0007.
  */
-// var url = "http://192.168.1.12:8080/streamplatserver/";  //liwang
-var url = "http://192.168.1.92:8083/streamplatserver/";
+var url = "http://192.168.1.12:8080/streamplatserver/";  //liwang
+// var url = "http://192.168.1.92:8083/streamplatserver/";
 var ERR_OK = 0;
